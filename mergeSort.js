79d2401b5813d4d035e8afc175c5c51062449c6a
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-02-14 21:35:17 +0200

    Add function to sort halves

---
+++ mergeSort.js
@@ -0,0 +1,9 @@
+function mergeSort(arr) {
+  if (arr.length < 2) {
+    console.log(arr);
+    return arr;
+  }
+  const leftHalf = arr.slice(0, Math.floor(arr.length / 2));
+  const rightHalf = arr.slice(Math.floor(arr.length / 2));
+  
+}
